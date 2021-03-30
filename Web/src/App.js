86d80fa6532da7a0113fa95ce5b91c519c11f897
Alex Rocha <alexsandrofernandes@setic.ro.gov.br>
2021-03-30 12:31:51 -0400

    Ajustando o front

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,174 +1,70 @@
-import React, {Component} from 'react';
-import classNames from 'classnames';
-import {AppTopbar} from './AppTopbar';
-import { AppFooter} from './AppFooter';
-import { AppMenu } from './AppMenu';
-import { AppProfile} from './AppProfile';
-import { Route} from 'react-router-dom';
-import { Dashboard } from './components/Dashboard';
-import { Login } from './components/Login';
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
-import './layout/layout.scss';
-import './App.scss';
-import { ClienteConsulta } from './components/Cliente/ClienteConsulta';
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import ModalForm from './components/Modals/ModalForm'
+import DataTable from './components/Tables/DataTable'
+import { CSVLink } from "react-csv"
 
 class App extends Component {
+  state = {
+    items: []
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            layoutMode: 'static',
-            layoutColorMode: 'dark',
-            staticMenuInactive: false,
-            overlayMenuActive: false,
-            mobileMenuActive: false
-        };
+  getItems(){
+    fetch('http://localhost:53700/api/clientes')
+      .then(response => response.json())
+      .then(items => this.setState({items}))
+      .catch(err => console.log(err))
+  }
 
-        this.onWrapperClick = this.onWrapperClick.bind(this);
-        this.onToggleMenu = this.onToggleMenu.bind(this);
-        this.onSidebarClick = this.onSidebarClick.bind(this);
-        this.onMenuItemClick = this.onMenuItemClick.bind(this);
-        this.createMenu();
-    }
+  addItemToState = (item) => {
+    this.setState(prevState => ({
+      items: [...prevState.items, item]
+    }))
+  }
 
-    onWrapperClick(event) {
-        if (!this.menuClick) {
-            this.setState({
-                overlayMenuActive: false,
-                mobileMenuActive: false
-            });
-        }
+  updateState = (item) => {
+    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
+    const newArray = [
+    // destructure all items from beginning to the indexed item
+      ...this.state.items.slice(0, itemIndex),
+    // add the updated item to the array
+      item,
+    // add the rest of the items to the array from the index after the replaced item
+      ...this.state.items.slice(itemIndex + 1)
+    ]
+    this.setState({ items: newArray })
+  }
 
-        this.menuClick = false;
-    }
+  deleteItemFromState = (id) => {
+    const updatedItems = this.state.items.filter(item => item.id !== id)
+    this.setState({ items: updatedItems })
+  }
 
-    onToggleMenu(event) {
-        this.menuClick = true;
+  componentDidMount(){
+    this.getItems()
+  }
 
-        if (this.isDesktop()) {
-            if (this.state.layoutMode === 'overlay') {
-                this.setState({
-                    overlayMenuActive: !this.state.overlayMenuActive
-                });
-            }
-            else if (this.state.layoutMode === 'static') {
-                this.setState({
-                    staticMenuInactive: !this.state.staticMenuInactive
-                });
-            }
-        }
-        else {
-            const mobileMenuActive = this.state.mobileMenuActive;
-            this.setState({
-                mobileMenuActive: !mobileMenuActive
-            });
-        }
-       
-        event.preventDefault();
-    }
-
-    onSidebarClick(event) {
-        this.menuClick = true;
-    }
-
-    onMenuItemClick(event) {
-        if(!event.item.items) {
-            this.setState({
-                overlayMenuActive: false,
-                mobileMenuActive: false
-            })
-        }
-    }
-
-    createMenu() {
-        this.menu = [
-            {label: 'Login', icon: 'pi pi-fw pi-home', command: () => {window.location = '#/login'}},
-            {label: 'Dashboard', icon: 'pi pi-fw pi-home', command: () => {window.location = '#/'}},
-            {
-                label: 'Cliente', 
-                icon: 'pi pi-fw pi-sliders-h', 
-                items: [
-                    {label: 'Consultar', icon: 'pi pi-fw pi-th-large', to: '/clienteconsulta'}
-                ]
-            }
-            
-        ];
-    }
-
-    addClass(element, className) {
-        if (element.classList)
-            element.classList.add(className);
-        else
-            element.className += ' ' + className;
-    }
-
-    removeClass(element, className) {
-        if (element.classList)
-            element.classList.remove(className);
-        else
-            element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
-
-    isDesktop() {
-        return window.innerWidth > 1024;
-    }
-
-    componentDidUpdate() {
-        if (this.state.mobileMenuActive)
-            this.addClass(document.body, 'body-overflow-hidden');
-        else
-            this.removeClass(document.body, 'body-overflow-hidden');
-    }
-
-    render() {
-        const logo = this.state.layoutColorMode === 'dark' ? 'assets/layout/images/logo.png': 'assets/layout/images/logo.png';
-
-        const wrapperClass = classNames('layout-wrapper', {
-            'layout-overlay': this.state.layoutMode === 'overlay',
-            'layout-static': this.state.layoutMode === 'static',
-            'layout-static-sidebar-inactive': this.state.staticMenuInactive && this.state.layoutMode === 'static',
-            'layout-overlay-sidebar-active': this.state.overlayMenuActive && this.state.layoutMode === 'overlay',
-            'layout-mobile-sidebar-active': this.state.mobileMenuActive
-        });
-
-        const sidebarClassName = classNames("layout-sidebar", {
-            'layout-sidebar-dark': this.state.layoutColorMode === 'dark',
-            'layout-sidebar-light': this.state.layoutColorMode === 'light'
-        });
-
-        return (
-            <div className={wrapperClass} onClick={this.onWrapperClick}>
-                <AppTopbar onToggleMenu={this.onToggleMenu}/>
-
-                <div ref={(el) => this.sidebar = el} className={sidebarClassName} onClick={this.onSidebarClick}>
-                    <div className="layout-logo">
-                        <img alt="Logo" src={logo} width="160"/>
-                    </div>
-                    <AppProfile />
-                    <AppMenu model={this.menu} onMenuItemClick={this.onMenuItemClick} />
-                </div>
-
-                <div className="layout-main">
-                    <Route path="/" exact component={Dashboard} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/clienteconsulta" component={ClienteConsulta} />
-                </div>
-
-                <AppFooter />
-
-                <div className="layout-mask"></div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1 style={{margin: "20px 0"}}>Clientes</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mb-3">
+            <ModalForm buttonLabel="Add Item" addItemToState={this.addItemToState}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
-
-
-
-export default App;
+export default App
